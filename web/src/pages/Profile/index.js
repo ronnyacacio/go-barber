a6f7fb68aba25774784083f7ad8624ad04eb49b1
Ronny Acacio <ronnyacacio27@gmail.com>
2020-04-28 23:15:27 -0300

    update profile complete

--- web/src/pages/Profile/index.js
+++ web/src/pages/Profile/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 
+import AvatarInput from './AvatarInput';
+import { updateProfileRequest } from '~/store/modules/user/actions';
 import { Container } from './styles';
 
 export default function Profile() {
+  const dispatch = useDispatch();
   const profile = useSelector((state) => state.user.profile);
 
-  function handleSubmit() {}
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
 
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
+
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
 

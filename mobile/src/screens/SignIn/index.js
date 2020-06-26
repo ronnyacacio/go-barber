5c8f0cdb9a422276b11f29755f7e1c15c41d409d
Ronny Acacio <ronnyacacio27@gmail.com>
2020-06-26 13:23:19 -0300

    feat: add splash screen and signIn styles

--- mobile/src/screens/SignIn/index.js
+++ mobile/src/screens/SignIn/index.js
@@ -1,16 +1,45 @@
 import React from 'react';
 
-import { Background, Input, Button } from '../../components';
+import { Background } from '../../components';
+import logo from '../../assets/logo.png';
+import {
+  Container,
+  Img,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
 
 export default function SignIn() {
   return (
     <Background>
-      <Input
-        style={{ marginTop: 30 }}
-        icon="call"
-        placeholder="Digite seu nome"
-      />
-      <Button>Entrar</Button>
+      <Container>
+        <Img source={logo} />
+
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha secreta"
+          />
+
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+        </Form>
+
+        <SignLink>
+          <SignLinkText>Criar conta gratuita</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }

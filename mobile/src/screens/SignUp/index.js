c9174bb70e6baaa758795d41ee3a4a2b143b5e2f
Ronny Acacio <ronnyacacio27@gmail.com>
2020-06-26 13:30:02 -0300

    feat: signUp styles

--- mobile/src/screens/SignUp/index.js
+++ mobile/src/screens/SignUp/index.js
@@ -1,12 +1,52 @@
 import React from 'react';
-import { View } from 'react-native';
 
 import { Background } from '../../components';
+import logo from '../../assets/logo.png';
+import {
+  Container,
+  Img,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
 
-export default function SignUp() {
+export default function SignUp({ navigation }) {
   return (
     <Background>
-      <View />
+      <Container>
+        <Img source={logo} />
+
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Nome completo"
+          />
+
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha secreta"
+          />
+
+          <SubmitButton onPress={() => {}}>Criar conta</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => navigation.navigate('SignIn')}>
+          <SignLinkText>Voltar</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }

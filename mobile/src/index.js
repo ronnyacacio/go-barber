ded22f91ba3c8c3fa9c878b8f437ffa848a14381
Ronny Acacio <ronnyacacio27@gmail.com>
2020-06-26 14:10:24 -0300

    feat: config store with redux and sagas

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
+import { store, persistor } from './store';
 import Routes from './routes';
 
 export default function App() {
   return (
-    <>
-      <StatusBar backgroundColor="transparent" translucent />
-      <Routes />
-    </>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <StatusBar backgroundColor="transparent" translucent />
+        <Routes />
+      </PersistGate>
+    </Provider>
   );
 }

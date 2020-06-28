e3192a90502e6d4271da180083e6e276458bf3d2
Ronny Acacio <ronnyacacio27@gmail.com>
2020-06-28 09:13:17 -0300

    feat: screen profile and update user

---
+++ mobile/src/screens/Profile/styles.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components/native';
+import Constants from 'expo-constants';
+
+import { Input, Button } from '../../components';
+
+const { statusBarHeight } = Constants;
+
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const Title = styled.Text`
+  font-size: 20px;
+  color: #fff;
+  font-weight: bold;
+  align-self: center;
+  margin-top: ${statusBarHeight + 30}px;
+`;
+
+export const Form = styled.ScrollView.attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: { padding: 30 },
+})`
+  align-self: stretch;
+`;
+
+export const FormInput = styled(Input)`
+  margin-bottom: 10px;
+`;
+
+export const Separator = styled.View`
+  height: 1px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 20px 0 30px;
+`;
+
+export const SubmitButton = styled(Button)`
+  margin-top: 5px;
+`;
